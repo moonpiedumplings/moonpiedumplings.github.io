3278cccb1e1ef3e11d903b13e0d04f6414f0a64b
moonpiedumplings <jeff.fonseca777@gmail.com>
2023-06-30 22:13:57 +0200

    progress on openstack

--- projects/openstack-on-nixos/mach-nix/keystone.nix
+++ projects/openstack-on-nixos/mach-nix/keystone.nix
@@ -1,18 +1,18 @@
 {
-    pkgs ? import <nipxkgs>,
-    #fetchFromGithub,
+    pkgs ? import <nipxkgs> {}, 
+    #pkgs ? (import fetchtarball {"https://github.com/nixos/nixpkgs/archive/f10cdcf31dd2a436edbf7f0ad82c44b911804bc8.tar.gz"}),
     fetchgit,
     versionPy ? "39"
 } : 
 
 let
-    mach-nix = import (fetchgit {
+    python = pkgs."python${versionPy}Full";
+    pythonPackages = pkgs."python${versionPy}Packages";
+    mach-nix = import (fetchGit {
     url = "https://github.com/DavHau/mach-nix";
     rev = "65266b5cc867fec2cb6a25409dd7cd12251f6107";
-    sha256 = "sha256-1OBBlBzZ894or8eHZjyADOMnGH89pPUKYGVVS5rwW/0=";
-    }) {};
-    python = pkgs."python${versionPy}Full";
-    pythonPkgs = pkgs."python${versionPy}Packages";
+    #sha256 = "sha256-1OBBlBzZ894or8eHZjyADOMnGH89pPUKYGVVS5rwW/0=";
+    }) {inherit pkgs python pythonPackages;};
 in
 
 mach-nix.buildPythonApplication {
@@ -24,4 +24,6 @@ mach-nix.buildPythonApplication {
         url = "https://github.com/openstack/keystone/";
         rev = "040e6d09b1e7e6817c81209c2b089d318715bef6";
     };
+    pypiDataRev = "e9571cac25d2f509e44fec9dc94a3703a40126ff";
+    pypiDataSha256 = "1rbb0yx5kjn0j6lk0ml163227swji8abvq0krynqyi759ixirxd5";
 }
