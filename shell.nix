554d41434cef80831e82d203c0fbb3465ab54da8
moonpiedumplings <jeff.fonseca777@gmail.com>
2023-11-09 00:22:26 -0800

    fixed shell.nix
    bqplot wouldnt build on nix installed via determinatesystems and channels in root. I don't know why

--- shell.nix
+++ shell.nix
@@ -2,11 +2,17 @@ let
     pkgs = import <nixpkgs> {};
     quarto = pkgs.callPackage ./env/quarto.nix {};
     python3 = pkgs.python311;
-    pythonDeps = ps: with ps; [ jupyter notebook qtconsole jupyter_console nbconvert ipykernel ipywidgets matplotlib bqplot pip ];
+    pythonDeps = ps: with ps; [ 
+		jupyter notebook qtconsole jupyter_console 
+		nbconvert 
+		ipykernel ipywidgets matplotlib 
+		#bqplot 
+		pip ];
 in
     pkgs.mkShell {
         QUARTO_PYTHON = "${pkgs.python3.withPackages pythonDeps}/bin/python3";
         packages = with pkgs; [ 
+	    nodePackages_latest.npm
             (python3.withPackages pythonDeps)
             quarto
             texlive.combined.scheme-full
