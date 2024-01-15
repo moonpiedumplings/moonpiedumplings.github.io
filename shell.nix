360099c65a0b7b403dd8e7cb7c5eba8925345520
moonpiedumplings <jeff.fonseca777@gmail.com>
2024-01-15 04:57:24 -0800

    workaround for quarto visual editing

--- shell.nix
+++ shell.nix
@@ -1,18 +1,41 @@
 let
     pkgs = import <nixpkgs> {};
-    #quarto = pkgs.callPackage ./env/quarto.nix {};
+
     python3 = pkgs.python311;
     pythonDeps = ps: with ps; [ 
-		jupyter notebook qtconsole jupyter_console 
-		nbconvert 
-		ipykernel ipywidgets matplotlib 
-		#bqplot 
-		pip ];
+		jupyter #notebook qtconsole jupyter_console
+		# nbconvert
+		# ipykernel ipywidgets
+		# matplotlib
+		pip
+
+		];
+     quarto = pkgs.quarto.overrideAttrs (oldAttrs: rec {
+         preFixup = ''
+          wrapProgram $out/bin/quarto \
+            --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.deno ]} \
+            --prefix QUARTO_ESBUILD : ${pkgs.esbuild}/bin/esbuild \
+            --prefix QUARTO_DART_SASS : ${pkgs.dart-sass}/bin/dart-sass \
+            --prefix QUARTO_PYTHON : "${pkgs.python3.withPackages pythonDeps}/bin/python3" \
+          '';
+          installPhase = ''
+            runHook preInstall
+
+            mkdir -p $out/bin $out/share
+
+            mv bin/* $out/bin
+            mv share/* $out/share
+
+            runHook postInstall
+        '';
+    });
 in
     pkgs.mkShell {
+        PYTHONPATH = "${pkgs.python3.withPackages pythonDeps}/bin/python3";
         QUARTO_PYTHON = "${pkgs.python3.withPackages pythonDeps}/bin/python3";
+        QUARTO_PANDOC = "${quarto}/bin/tools/pandoc";
         packages = with pkgs; [ 
-	    nodePackages_latest.npm
+            nodePackages_latest.npm
             (python3.withPackages pythonDeps)
             quarto
             texlive.combined.scheme-full
