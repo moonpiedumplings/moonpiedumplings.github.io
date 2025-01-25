b2467ed39a6b9190e270197371f535d05b74a5d7
moonpiedumplings <jeff.fonseca777@gmail.com>
2025-01-24 16:24:29 -0800

    update nix versions

--- devshell.nix
+++ devshell.nix
@@ -27,10 +27,10 @@ let
 
   quarto = pkgs.quarto.overrideAttrs (oldAttrs: rec {
     # 1.3 + newer (I think) has a weird bug with the text boxes where they are white on a black background. Readable, but ugly
-    version = "1.5.57";
+    version = "1.6.40";
     src = pkgs.fetchurl {
       url = "https://github.com/quarto-dev/quarto-cli/releases/download/v${version}/quarto-${version}-linux-amd64.tar.gz";
-      sha256 = "sha256-ZBjv/Z98il8EMZe88fMKSi1YjeOZ8jEh7OxYDKUTMpY=";
+      sha256 = "sha256-d74YsCtBSEdxSuqnPRo6OXxCnF118L69d99cnPl05/o=";
     };
     preFixup = ''
       wrapProgram $out/bin/quarto \
